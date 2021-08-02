ae1a6ae2860cf65087af38d3be11d6de5f267b33
Ankit <ph,official.ankit@gmail.com>
2021-08-02 21:10:05 +0530

    validate api request

--- server/src/controller/auth.js
+++ server/src/controller/auth.js
@@ -1,90 +1,93 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 // local import
-const User = require('../models/user');
+const User = require("../models/user");
 
 exports.signup = (req, res) => {
-    User.findOne({
-        email: req.body.email
-    }).exec((err, user) => {
-        if(user)
-            return res.status(400).json({
-                message: 'User already registered'
-            });
 
-        if(err)
-            console.log(err);
+  User.findOne({
+    email: req.body.email,
+  }).exec((err, user) => {
+    if (user)
+      return res.status(400).json({
+        message: "User already registered",
+      });
 
-        // new user in database
-        const { firstName, lastName, email, password } = req.body; // destructuring
-        const _user = new User({
-            firstName, 
-            lastName, 
-            email, 
-            password,
-            username: Math.random().toString()
-        });
+    if (err) console.log(err);
 
-        _user.save((err, data) => {
-            if(err)
-                return res.status(400).json({
-                    message: 'Something went wrong!'
-                });
-            
-            if(data) {
-                return res.status(201).json({
-                    // user: data
-                    message: 'User created successfully..!'
-                });
-            }
-
-        });
+    // new user in database
+    const { firstName, lastName, email, password } = req.body; // destructuring
+    const _user = new User({
+      firstName,
+      lastName,
+      email,
+      password,
+      username: Math.random().toString(),
     });
-}
+
+    _user.save((err, data) => {
+      if (err)
+        return res.status(400).json({
+          message: "Something went wrong!",
+        });
+
+      if (data) {
+        return res.status(201).json({
+          // user: data
+          message: "User created successfully..!",
+        });
+      }
+    });
+  });
+};
 
 exports.signin = (req, res) => {
-    User.findOne({ email: req.body.email })
-    .exec((err, user) => {
-        if(err)
-            return res.status(400).json({ err });
-        if(user) {
-            if(user.authenticate(req.body.password)) {
-                // token
-                const token = jwt.sign({
-                    _id: user._id
-                }, process.env.JWT_SECRET, {
-                    expiresIn: '1h'
-                });
+  User.findOne({ email: req.body.email }).exec((err, user) => {
+    if (err) return res.status(400).json({ err });
+    if (user) {
+      if (user.authenticate(req.body.password)) {
+        // token
+        const token = jwt.sign(
+          {
+            _id: user._id,
+          },
+          process.env.JWT_SECRET,
+          {
+            expiresIn: "1h",
+          }
+        );
 
-                // destructure user
-                const { _id, firstName, lastName, email, role, fullName } = user;
+        // destructure user
+        const { _id, firstName, lastName, email, role, fullName } = user;
 
-                res.status(200).json({
-                    token,
-                    user: {
-                        _id, firstName, lastName, email, role, fullName
-                    }
-                });
-            }
-            else {
-                return res.status(400).json({
-                    message: 'Invalid password'
-                });
-            }
-        }
-        else {
-            return res.status(400).json({
-                message: 'Something went wrong'
-            });
-        }
-    });
-}
+        res.status(200).json({
+          token,
+          user: {
+            _id,
+            firstName,
+            lastName,
+            email,
+            role,
+            fullName,
+          },
+        });
+      } else {
+        return res.status(400).json({
+          message: "Invalid password",
+        });
+      }
+    } else {
+      return res.status(400).json({
+        message: "Something went wrong",
+      });
+    }
+  });
+};
 
 exports.requireSignin = (req, res, next) => {
-    const token = req.headers.authorization.split(" ")[1];
-    // console.log(token);
-    const user = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = user;
-    next();
-    
-}
+  const token = req.headers.authorization.split(" ")[1];
+  // console.log(token);
+  const user = jwt.verify(token, process.env.JWT_SECRET);
+  req.user = user;
+  next();
+};
